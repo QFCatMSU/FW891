2abd2634dc28043ef3d1a3d34c22e7bc41808289
belinskyc <36079242+belinskyc@users.noreply.github.com>
2023-03-13 11:11:18 -0400

    Updates to lake erie shapefiles

--- scripts/LakeErieTest.R
+++ scripts/LakeErieTest.R
@@ -11,13 +11,14 @@
   library(package = "ggOceanMaps");
   # library(package = "dplyr");
   library(package = "sf");       # Simple Features
-  # library(package = "rnaturalearth");     # for getting coord data
-  # library(package = "rnaturalearthdata"); # for getting coord data
+  library(package = "rgeos");    # getting/converting crs
+  library(package = "rgdal");    # getting/converting crs
   
   # Lake MI is a KML file downloaded for the MI arcgis website
   lakeErie = st_read(dsn="shapefiles/Lake_Erie/Lake_Erie_Contours.shp");
   lakeErie_SF = st_as_sf(lakeErie); 
   
+  st_write(lakeErie_SF, "shapefiles/LakeErie.geojson", driver="GeoJSON")
   plot1 = ggplot() +
     geom_contour(data = lakeErie_SF,
             mapping = aes(geometry=geometry,
--- scripts/googleMAps.R
+++ scripts/googleMAps.R
@@ -4,18 +4,45 @@
   library(package = "viridis");
   library(package = "sf"); 
   
+  ### Reduce size of shapefiles here: https://mapshaper.org/
+  ### I have not found a realiable R way to simplify bathymetric data
+  
   ggmap::register_google("AIzaSyAzE_ptJ4iJvpaTaUly3wr6CrZrLiWixzU");
   
-  # bathymetry data downloaded from NOAA
+  # original bathymetry data downloaded from NOAA (this is slow...)
   lakeErie = st_read(dsn="shapefiles/Lake_Erie/Lake_Erie_Contours.shp");
-  lakeErie_SF = st_as_sf(lakeErie); 
+  lakeErie_SF = st_as_sf(lakeErie);  # convert to simple feature (not always needed)
   
-  plot3 = ggmap(get_map("Lake Erie", 
+  # same bathymetry data reduced by 95% using mapshaper.org
+  lakeErieReduced = st_read(dsn="shapefiles/Lake_Erie/reduced/Lake_Erie_Contours.shp");
+  lakeErieReduced_SF = st_as_sf(lakeErieReduced); 
+  
+  ### Plotting google map with original Lake Erie bathymetry data
+  plot1 = ggmap(get_map("Lake Erie", 
                         zoom = 8, maptype = "terrain")) + 
-          geom_sf(data = lakeErie_SF,
+    geom_sf(data = lakeErie_SF, 
+            mapping = aes(geometry=geometry, 
+                          color=DEPTH),
+            inherit.aes = FALSE) +       # don't inherit ggmap mappings
+    scale_color_viridis(option="D");     # for contour line color scheme
+  # plot(plot1);
+  
+  ### Plotting google map with reduced Lake Erie bathymetry data
+  plot2 = ggmap(get_map("Lake Erie", 
+                        zoom = 8, maptype = "terrain")) + 
+          geom_sf(data = lakeErieReduced_SF, 
                   mapping = aes(geometry=geometry, 
                                 color=DEPTH),
                   inherit.aes = FALSE) +       # don't inherit ggmap mappings
           scale_color_viridis(option="D");     # for contour line color scheme
-  plot(plot3);
+  # plot(plot2);
+  
+  ### Test changing CRS (kind of a random CRS)
+  #   This will not maintain the google map image 
+  plot3  = plot2 + 
+    coord_sf(crs = 26917,    # UTM zone 17N
+             xlim = c(300000, 670000),  
+             ylim = c(4550000, 4750000),
+             expand = TRUE);
+  # plot(plot3);
 } 
